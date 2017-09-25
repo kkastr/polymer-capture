63c5d576587c30ce6ae15fcf55612a9bb98d40c9
Konstantinos Kastritis <dinokastritis@gmail.com>
2017-09-25 08:04:11 -0400

    code ver with less events per job

---
+++ evolve-g0-2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+rseed_suffix=123456
+mem="1G"
+filename="grid2"
+
+
+mkdir -p logs
+mkdir -p data
+
+
+for N in 10 20 50 100
+do
+
+	mkdir -p data/${filename}_$N
+	mkdir -p logs/${filename}_$N
+
+	
+ 	
+	for i in {00..10}
+	do
+		rseed=${N}${i}${rseed_suffix}
+		time="$(python primefactors.py $N):00:00"
+		jobname="grid2_${N}_${rseed}"
+		log="logs/${filename}_$N/log_$rseed.out"
+		sbatch --mem=$mem --time=$time --job-name=$jobname --output=$log graham-job-submit.sh $N $rseed $filename
+	done
+
+done
+		
+
